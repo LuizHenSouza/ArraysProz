53da29b9fb9473696ec9b0161f535522a3734a2e
Luiz Souza <LuizCraftPB@Gmail.com>
2024-03-20 18:06:57 -0300

    Implementação da solução

---
+++ Main.py
@@ -0,0 +1,10 @@
+# Declare dois arrays, cada um com um mínimo de cinco elementos, e imprima eles no terminal usando o comando print(). O primeiro array deve conter os produtos de uma loja da sua escolha (loja de comida, materiais de construção, música, etc). O segundo array deve conter os anos de nascimento de familiares e amigos seus. Lembre-se de usar nomes descritivos para nomear cada variável, e de usar o tipo de dado apropriado para cada lista (strings, booleanos, números inteiros, floats).
+
+ListaDeProdutosLoja = ["Cimento", "Fita isolante", "Tijolos", "Tinta", "Fios de cobre encapados"]
+ListaDeDataNascimento = ["09/09/2002", "18/11/1999", "12/11/1998", "20/06/1972", "21/06/1974"]
+
+for A in ListaDeProdutosLoja:
+    print(A)
+
+for B in ListaDeDataNascimento:
+    print(B)
